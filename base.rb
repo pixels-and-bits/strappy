1ea284f10d7fd1cbec15ed57720c6bfbb3ba012c
Michael Moen <michael@underpantsgnome.com>
2009-02-08 12:43:08 -0600

    add authlogic gem and remove restful_auth generator

--- base.rb
+++ base.rb
@@ -85,44 +85,6 @@ rake('db:migrate')
 git :add => "."
 git :commit => "-a -m 'Added AR Sessions'"
 
-# RESTful Auth
-inside('vendor/plugins') do
-  run 'rm -rf restful_authentication' if File.exist?('restful_authentication')
-  run 'rm -rf restful-authentication' if File.exist?('restful-authentication')
-end
-
-plugin 'restful_authentication',
-  :git => 'git://github.com/UnderpantsGnome/restful_authentication.git'
-
-run './script/generate authenticated user session \
---include-activation \
---aasm \
---rspec'
-
-# add in the user_observer
-environment 'config.active_record.observers = :user_observer'
-
-# for some reason rails complains about AASM unless we have this
-gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com'
-
-# Sassify the templates
-inside('app/views/users/') do
-  run 'html2haml -r new.html.erb new.html.haml && rm new.html.erb'
-  # no idea what this file is...
-  run 'rm _user_bar.html.erb' if File.exist?('_user_bar.html.erb')
-end
-
-inside('app/views/sessions/') do
-  run 'html2haml -r new.html.erb new.html.haml && rm new.html.erb'
-end
-
-# add routes
-route open("#{SOURCE}/config/restful_auth_routes.rb").read
-
-rake('db:migrate')
-git :add => "."
-git :commit => "-a -m 'Added RESTful Authentication'"
-
 # jRails
 plugin 'jrails', :svn => 'http://ennerchi.googlecode.com/svn/trunk/plugins/jrails'
 
@@ -193,7 +155,5 @@ git :add => "."
 git :commit => "-a -m 'Removed index.html. Added HomeController'"
 
 puts "\n#{'*' * 80}\n\n"
-puts "Be sure to clean up the views converted to Haml (fix indenting, remove - end)"
-puts "  app/views/sessions/new.html.haml"
-puts "  app/views/users/new.html.haml"
+puts "All done. Enjoy."
 puts "\n#{'*' * 80}\n\n"
