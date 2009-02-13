fb09b6575f4d1ca5e4e65b089deea0275b60dd4a
Michael Moen <michael@underpantsgnome.com>
2009-02-13 12:11:36 -0600

    fix the accidental deletion of jrails.js

--- base.rb
+++ base.rb
@@ -86,7 +86,10 @@ plugin 'jrails', :svn => 'http://ennerchi.googlecode.com/svn/trunk/plugins/jrail
 
 # remove the installed files, we're using a newer version below
 inside('public/javascripts') do
-  %w( jquery-ui.js jquery.js ).each do |file|
+  %w(
+    jquery-ui.js
+    jquery.js
+  ).each do |file|
     run "rm -f #{file}"
   end
 end
@@ -95,7 +98,19 @@ git :add => "."
 git :commit => "-a -m 'Added jRails plugin'"
 
 # jQuery
-git :rm => "public/javascripts/*"
+
+# clean up prototype files
+inside('public/javascripts') do
+  %w(
+    application.js
+    controls.js
+    dragdrop.js
+    effects.js
+    prototype.js
+  ).each do |file|
+    run "rm -f #{file}"
+  end
+end
 
 file 'public/javascripts/jquery.js',
   open('http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js').read
@@ -110,7 +125,6 @@ file 'public/javascripts/jquery.form.js',
 
 file "public/javascripts/application.js", <<-JS
 $(function() {
-  // ...
 });
 JS
 
