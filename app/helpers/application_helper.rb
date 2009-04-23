5923403e6c73a3f44c6ee0fc33b3f55399033b73
Michael Moen <michael@underpantsgnome.com>
2009-04-23 16:14:55 -0500

    put the silly hack for rcov back in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def html_attrs(lang = 'en-US')
-    {:xmlns => "http://www.w3.org/1999/xhtml", 'xml:lang' => lang, :lang => lang}
+  # dirty ugly hack to get rcov to see this
+  def html_attrs(lang = 'en-US'); {:xmlns => "http://www.w3.org/1999/xhtml", 'xml:lang' => lang, :lang => lang}
   end
 
   def http_equiv_attrs
