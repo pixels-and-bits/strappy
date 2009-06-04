3466d2dc9b072d879822993f5af3772b58203e68
Michael Moen <michael@underpantsgnome.com>
2009-06-03 19:38:43 -0500

    more spec helpers

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -12,3 +12,32 @@ def silence_stdout(&block)
   yield
   $stdout = stdout
 end
+
+def paginated(ary)
+  WillPaginate::Collection.create(1, 10) do |pager|
+    pager.replace(ary)
+  end
+end
+
+class UseLayout
+   def initialize(expected)
+     @expected = 'layouts/' + expected
+   end
+
+   def matches?(controller)
+     @actual = controller.layout
+     @actual == @expected
+   end
+
+   def failure_message
+     return "use_layout expected #{@expected.inspect}, got #{@actual.inspect}", @expected, @actual
+   end
+
+   def negeative_failure_message
+     return "use_layout expected #{@expected.inspect} not to equal #{@actual.inspect}", @expected, @actual
+   end
+end
+
+def use_layout(expected)
+   UseLayout.new(expected)
+end
