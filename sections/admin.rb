cac152f4eb1ef6c6b4d08da25eacca967dca4c3e
Michael Moen <michael@underpantsgnome.com>
2010-10-29 19:36:33 -0500

    admin section tweaks

--- sections/admin.rb
+++ sections/admin.rb
@@ -1,4 +1,24 @@
 generate 'devise admin'
+generate 'controller admin/base index'
+
+# fix routes
+file_str_replace 'config/routes.rb',
+  'get "base/index"',
+  %Q{
+  namespace :admin do
+    root :to => 'base#index'
+  end # namespace : admin
+  }
+
+file_str_replace 'config/routes.rb',
+  'devise_for :admins',
+  'devise_for :admin'
+
+# force auth
+file_inject 'app/controllers/admin/base_controller.rb',
+  'class Admin::BaseController < ApplicationController', 
+  '  before_filter :authenticate_admin!
+'
 
 git :add => "."
 git :commit => "-am 'Added admin'"
