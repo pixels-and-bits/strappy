7ee27e72925e4bdc3eaada4fea8a9f9e231bbcc9
Michael Moen <michael@underpantsgnome.com>
2010-11-12 19:13:45 -0600

    remove old haml views

--- sections/html5-boilerplate.rb
+++ sections/html5-boilerplate.rb
@@ -1,3 +1,5 @@
+git :rm => 'app/views/layouts/application.html.erb'
+
 run 'bundle exec compass init rails -r html5-boilerplate -u html5-boilerplate --force --sass-dir app/stylesheets --css-dir public/stylesheets'
 
 git :add => "."
